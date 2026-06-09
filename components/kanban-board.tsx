5c3c264c385148c2518074d21f6e9dfa95c4c187
Mohsen Ebrahimi <Mohsen.eb1381@gmail.com>
2026-06-09 15:26:54 +0330

    Create COLUMN_CONFIG array

--- components/kanban-board.tsx
+++ components/kanban-board.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import { Board } from "@/lib/models/models.type";
+import { Award, Calendar, CheckCircle2, Mic, XCircle } from "lucide-react";
+
 type KanbanBoardProps = {
-  board: any;
+  board: Board;
   userId: string;
 };
+interface ColConfig {
+  color: string;
+  icon: React.ReactNode;
+}
+const COLUMN_CONFIG: Array<ColConfig> = [
+  {
+    color: "bg-cyan-500",
+    icon: <Calendar className="h-4 w-4" />,
+  },
+  {
+    color: "bg-purple-500",
+    icon: <CheckCircle2 className="h-4 w-4" />,
+  },
+  {
+    color: "bg-green-500",
+    icon: <Mic className="h-4 w-4" />,
+  },
+  {
+    color: "bg-yellow-500",
+    icon: <Award className="h-4 w-4" />,
+  },
+  {
+    color: "bg-red-500",
+    icon: <XCircle className="h-4 w-4" />,
+  },
+];
 
 function KanbanBoard({ board, userId }: KanbanBoardProps) {
   return <div>KanbanBoard</div>;
